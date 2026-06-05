de6a91dc20a5b9a8b24301cc7ca9be507ba7f915
tanishkvamne4848 <tanishkvamne@gmail.com>
2026-06-06 00:35:43 +0530

    Update plot_run.py

--- dockflow1/plot_run.py
+++ dockflow1/plot_run.py
@@ -1,6 +1,6 @@
-from utils.plots import generate_score_distribution
+from plots import generate_score_distribution
 
 generate_score_distribution(
-    "results.csv",
-    "distribution.png"
+    "input_path/results.csv",
+    "output_path/distribution.png"
 )
